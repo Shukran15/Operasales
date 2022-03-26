2027ad0744b432612cf735d8123eae62c1bcf05c
ShukranBabaev <marina300@bk.ru>
2022-03-26 11:46:20 +0300

    lesson 20

--- src/main/java/com/example/Operasales/OperaRepozitory.java
+++ src/main/java/com/example/Operasales/OperaRepozitory.java
@@ -10,58 +10,59 @@ import java.util.Random;
 public class OperaRepozitory {
 
     @Autowired
-    private ArrayList <Primera> prs;
+    private ArrayList<Primera> prs;
 
-    public void addPr(Primera pr){
+    public void addPr(Primera pr) {
         prs.add(pr);
 
     }
 
     public void printAll() {
-        for(Primera primera: prs){
+        for (Primera primera : prs) {
             System.out.println(primera);
         }
     }
 
-    public void minusplace(long id){
-        for(Primera primera: prs) {
-            if(primera.getId() == id){
+    public void minusplace(long id) {
+        for (Primera primera : prs) {
+            if (primera.getId() == id) {
                 primera.setFreeplace(primera.getFreeplace() - 1);
             }
         }
     }
 
-    public void plusplace(long id){
-        for(Primera primera: prs) {
-            if(primera.getId() == id){
+    public void plusplace(long id) {
+        for (Primera primera : prs) {
+            if (primera.getId() == id) {
                 primera.setFreeplace(primera.getFreeplace() + 1);
             }
         }
     }
 
-    public void minusprimera(long id){
-        for(Primera primera: prs) {
-            if(primera.getId() == id){
-               prs.remove(primera);
+    public void minusprimera(long id) {
+        for (Primera primera : prs) {
+            if (primera.getId() == id) {
+                prs.remove(primera);
+                break;
             }
         }
     }
 
-    public void printPrimera (String name){
-        for(Primera primera: prs){
-            if (primera.getName().equals(name)){
+    public void printPrimera(String name) {
+        for (Primera primera : prs) {
+            if (primera.getName().equals(name)) {
                 System.out.println(primera);
             }
         }
 
     }
 
-    public Ticket buy(String name){
+    public Ticket buy(String name) {
 
         Random random = new Random();
         Ticket t = null;
-        for(Primera primera: prs){
-            if (primera.getName().equals(name)){
+        for (Primera primera : prs) {
+            if (primera.getName().equals(name)) {
                 int place = random.nextInt(120);
                 t = new Ticket(primera, 100, place);
 
@@ -72,12 +73,22 @@ public class OperaRepozitory {
         return t;
     }
 
-    public void vozvrat(Ticket ticket){
+    public void vozvrat(Ticket ticket) {
 
         Primera primera = ticket.getPr();
         long id = primera.getId();
         plusplace(id);
 
+    }
+
+    public Primera getPremieraById(long id) {
+        for (Primera primera : prs) {
+            if (primera.getId() == id) {
+                return primera;
+            }
+        }
+
+        return null;
     }
 
 }
--- src/main/java/com/example/Operasales/OperasalesApplication.java
+++ src/main/java/com/example/Operasales/OperasalesApplication.java
@@ -2,12 +2,39 @@ package com.example.Operasales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class OperasalesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OperasalesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(OperasalesApplication.class, args);
+
+        OperaRepozitory operaRepozitory = applicationContext.getBean(OperaRepozitory.class);
+
+
+        Primera newPr = new Primera(3, "Кармен", "Комик, Париж, Франция", Kategory.PG_13, 20);
+        operaRepozitory.addPr(newPr);
+
+        operaRepozitory.printAll();
+        System.out.println("Выберете id премьеру:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int id = scanner.nextInt();
+
+        Primera pr = operaRepozitory.getPremieraById(id);
+
+        Ticket ticket = operaRepozitory.buy(pr.getName());
+
+        System.out.println("Вы приобрели билет:");
+
+        System.out.println(ticket);
+
+        operaRepozitory.printAll();
+
+    }
 
 }
